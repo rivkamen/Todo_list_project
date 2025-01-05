3ce8875a90278ced2d89f566c0229070daad66a3
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-01-05 16:12:16 +0200

    Update Program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -27,8 +27,12 @@ builder.Services.AddCors(options =>
 });
 
 // Get the connection string from app configuration
+try{
 var connectionString = builder.Configuration.GetConnectionString("ToDoDB");
-
+}
+catch(error){
+    console.writeline("error:" + error);
+}
 if (string.IsNullOrEmpty(connectionString))
 {
     Console.WriteLine("Connection string is missing!");
