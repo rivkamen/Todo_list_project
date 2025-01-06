b0c96654b916451ca2c995d7c6fc97de1175f13a
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-01-06 20:39:33 +0200

    Update ToDoDbContext.cs

--- server/ToDoDbContext.cs
+++ server/ToDoDbContext.cs
@@ -1,4 +1,4 @@
-﻿// using System;
+// ﻿using System;
 // using System.Collections.Generic;
 // using Microsoft.EntityFrameworkCore;
 // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
@@ -41,136 +41,73 @@
 
 //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 // }
-// using System;
-// using System.Collections.Generic;
-// using Microsoft.EntityFrameworkCore;
-// using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
-
-// namespace TodoApi;
-
-// public partial class ToDoDbContext : DbContext
-// {
-//     // DbSet של משתמשים
-//     public virtual DbSet<User> Users { get; set; }
-
-//     public virtual DbSet<Item> Items { get; set; }
-
-//     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-//         : base(options)
-//     {
-//     }
-
-//     // הגדרת חיבור למסד הנתונים
-//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// {
-//     Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
-
-
-//     try
-// {
-//     var connectionString = Configuration.GetConnectionString("ToDoDB");
-//     optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-//     Console.WriteLine("success");
-// }
-// catch (Exception ex)
-// {
-//     Console.WriteLine($"Database connection failed: {ex.Message}");
-//     Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-// }
-
-
-// }
-
-//     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//     //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-
-//     // אפיון טבלאות ותכונות
-//     protected override void OnModelCreating(ModelBuilder modelBuilder)
-//     {
-//         modelBuilder
-//             .UseCollation("utf8mb4_0900_ai_ci")
-//             .HasCharSet("utf8mb4");
-
-//         // הגדרת טבלת Items
-//         modelBuilder.Entity<Item>(entity =>
-//         {
-//             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-//             entity.ToTable("items");
-
-//             entity.Property(e => e.Name).HasMaxLength(100);
-//         });
-
-//         // הגדרת טבלת Users (למשתמשים)
-//         modelBuilder.Entity<User>(entity =>
-//         {
-//             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-//             entity.ToTable("users");
-
-//             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
-//             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
-//         });
-
-//         OnModelCreatingPartial(modelBuilder);
-//     }
-
-//     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-// }
+using System;
+using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
 namespace TodoApi;
 
 public partial class ToDoDbContext : DbContext
 {
-    private readonly IConfiguration _configuration;
-
-    // Constructor to inject IConfiguration
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options, IConfiguration configuration)
-        : base(options)
-    {
-        _configuration = configuration;
-    }
-
+    // DbSet של משתמשים
     public virtual DbSet<User> Users { get; set; }
+
     public virtual DbSet<Item> Items { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+        : base(options)
     {
-        if (!optionsBuilder.IsConfigured)
-        {
-            Console.WriteLine("OnConfiguring is being called"); // Simple log
-            try
-            {
-                var connectionString = _configuration.GetConnectionString("ToDoDB");
-                optionsBuilder.UseMySql( "Server=bd2ivxjpmx5a7gpoxc5j-mysql.services.clever-cloud.com;User=uyb9xnwwuyygrbpc;Password=crvceLArTRBtAmA8rhKG;Database=bd2ivxjpmx5a7gpoxc5j", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-                Console.WriteLine("success");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Database connection failed: {ex.Message}");
-            }
-        }
     }
 
+    // הגדרת חיבור למסד הנתונים
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+{
+    Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
+
+
+    try
+{
+    var connectionString = Configuration.GetConnectionString("ToDoDB");
+    optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+    Console.WriteLine("success");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Database connection failed: {ex.Message}");
+    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+}
+
+
+}
+
+    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+    // אפיון טבלאות ותכונות
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
             .UseCollation("utf8mb4_0900_ai_ci")
             .HasCharSet("utf8mb4");
 
+        // הגדרת טבלת Items
         modelBuilder.Entity<Item>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
             entity.ToTable("items");
+
             entity.Property(e => e.Name).HasMaxLength(100);
         });
 
+        // הגדרת טבלת Users (למשתמשים)
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
             entity.ToTable("users");
+
             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
         });
