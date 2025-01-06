f0156d0dd52896adfad9487b59207b901802539d
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-01-06 20:04:48 +0200

    Update ToDoDbContext.cs

--- server/ToDoDbContext.cs
+++ server/ToDoDbContext.cs
@@ -41,73 +41,136 @@
 
 //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 // }
-using System;
-using System.Collections.Generic;
+// using System;
+// using System.Collections.Generic;
+// using Microsoft.EntityFrameworkCore;
+// using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// namespace TodoApi;
+
+// public partial class ToDoDbContext : DbContext
+// {
+//     // DbSet של משתמשים
+//     public virtual DbSet<User> Users { get; set; }
+
+//     public virtual DbSet<Item> Items { get; set; }
+
+//     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//         : base(options)
+//     {
+//     }
+
+//     // הגדרת חיבור למסד הנתונים
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// {
+//     Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
+
+
+//     try
+// {
+//     var connectionString = Configuration.GetConnectionString("ToDoDB");
+//     optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+//     Console.WriteLine("success");
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Database connection failed: {ex.Message}");
+//     Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+// }
+
+
+// }
+
+//     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//     //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+//     // אפיון טבלאות ותכונות
+//     protected override void OnModelCreating(ModelBuilder modelBuilder)
+//     {
+//         modelBuilder
+//             .UseCollation("utf8mb4_0900_ai_ci")
+//             .HasCharSet("utf8mb4");
+
+//         // הגדרת טבלת Items
+//         modelBuilder.Entity<Item>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("items");
+
+//             entity.Property(e => e.Name).HasMaxLength(100);
+//         });
+
+//         // הגדרת טבלת Users (למשתמשים)
+//         modelBuilder.Entity<User>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("users");
+
+//             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
+//             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
+//         });
+
+//         OnModelCreatingPartial(modelBuilder);
+//     }
+
+//     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// }
 using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+using Microsoft.Extensions.Configuration;
 
 namespace TodoApi;
 
 public partial class ToDoDbContext : DbContext
 {
-    // DbSet של משתמשים
-    public virtual DbSet<User> Users { get; set; }
+    private readonly IConfiguration _configuration;
 
-    public virtual DbSet<Item> Items { get; set; }
-
-    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+    // Constructor to inject IConfiguration
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options, IConfiguration configuration)
         : base(options)
     {
+        _configuration = configuration;
     }
 
-    // הגדרת חיבור למסד הנתונים
+    public virtual DbSet<User> Users { get; set; }
+    public virtual DbSet<Item> Items { get; set; }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-    Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            Console.WriteLine("OnConfiguring is being called"); // Simple log
+            try
+            {
+                var connectionString = _configuration.GetConnectionString("ToDoDB");
+                optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+                Console.WriteLine("success");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Database connection failed: {ex.Message}");
+            }
+        }
+    }
 
-
-    try
-{
-    var connectionString = Configuration.GetConnectionString("ToDoDB");
-    optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-    Console.WriteLine("success");
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Database connection failed: {ex.Message}");
-    Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-}
-
-
-}
-
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-
-    // אפיון טבלאות ותכונות
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
             .UseCollation("utf8mb4_0900_ai_ci")
             .HasCharSet("utf8mb4");
 
-        // הגדרת טבלת Items
         modelBuilder.Entity<Item>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("items");
-
             entity.Property(e => e.Name).HasMaxLength(100);
         });
 
-        // הגדרת טבלת Users (למשתמשים)
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
             entity.ToTable("users");
-
             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
         });
