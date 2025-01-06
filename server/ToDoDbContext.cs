7e6a703482c94289f0a47c312eaf775ce6131a86
rivkamen <159689577+rivkamen@users.noreply.github.com>
2025-01-07 00:12:02 +0200

    Update ToDoDbContext.cs

--- server/ToDoDbContext.cs
+++ server/ToDoDbContext.cs
@@ -1,4 +1,47 @@
-// // // ﻿using System;
+// // // // ﻿using System;
+// // // // using System.Collections.Generic;
+// // // // using Microsoft.EntityFrameworkCore;
+// // // // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+// // // // namespace TodoApi;
+
+// // // // public partial class ToDoDbContext : DbContext
+// // // // {
+// // // //     public ToDoDbContext()
+// // // //     {
+// // // //     }
+
+// // // //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+// // // //         : base(options)
+// // // //     {
+// // // //     }
+
+// // // //     public virtual DbSet<Item> Items { get; set; }
+
+// // // //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// // // //         => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+// // // //     protected override void OnModelCreating(ModelBuilder modelBuilder)
+// // // //     {
+// // // //         modelBuilder
+// // // //             .UseCollation("utf8mb4_0900_ai_ci")
+// // // //             .HasCharSet("utf8mb4");
+
+// // // //         modelBuilder.Entity<Item>(entity =>
+// // // //         {
+// // // //             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+// // // //             entity.ToTable("items");
+
+// // // //             entity.Property(e => e.Name).HasMaxLength(100);
+// // // //         });
+
+// // // //         OnModelCreatingPartial(modelBuilder);
+// // // //     }
+
+// // // //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// // // // }
+// // // using System;
 // // // using System.Collections.Generic;
 // // // using Microsoft.EntityFrameworkCore;
 // // // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
@@ -7,26 +50,49 @@
 
 // // // public partial class ToDoDbContext : DbContext
 // // // {
-// // //     public ToDoDbContext()
-// // //     {
-// // //     }
+// // //     // DbSet של משתמשים
+// // //     public virtual DbSet<User> Users { get; set; }
+
+// // //     public virtual DbSet<Item> Items { get; set; }
 
 // // //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
 // // //         : base(options)
 // // //     {
 // // //     }
 
-// // //     public virtual DbSet<Item> Items { get; set; }
-
+// // //     // הגדרת חיבור למסד הנתונים
 // // //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// // //         => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+// // // {
+// // //     Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
 
+
+// // //     try
+// // // {
+// // //     var connectionString = "Server=bd2ivxjpmx5a7gpoxc5j-mysql.services.clever-cloud.com;User=uyb9xnwwuyygrbpc;Password=crvceLArTRBtAmA8rhKG;Database=bd2ivxjpmx5a7gpoxc5j;";
+// // //     //Configuration.GetConnectionString("ToDoDB");
+// // //     optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+// // //     Console.WriteLine("success");
+// // // }
+// // // catch (Exception ex)
+// // // {
+// // //     Console.WriteLine($"Database connection failed: {ex.Message}");
+// // //     Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+// // // }
+
+
+// // // }
+
+// // //     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// // //     //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+// // //     // אפיון טבלאות ותכונות
 // // //     protected override void OnModelCreating(ModelBuilder modelBuilder)
 // // //     {
 // // //         modelBuilder
 // // //             .UseCollation("utf8mb4_0900_ai_ci")
 // // //             .HasCharSet("utf8mb4");
 
+// // //         // הגדרת טבלת Items
 // // //         modelBuilder.Entity<Item>(entity =>
 // // //         {
 // // //             entity.HasKey(e => e.Id).HasName("PRIMARY");
@@ -36,151 +102,161 @@
 // // //             entity.Property(e => e.Name).HasMaxLength(100);
 // // //         });
 
+// // //         // הגדרת טבלת Users (למשתמשים)
+// // //         modelBuilder.Entity<User>(entity =>
+// // //         {
+// // //             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+// // //             entity.ToTable("users");
+
+// // //             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
+// // //             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
+// // //         });
+
 // // //         OnModelCreatingPartial(modelBuilder);
 // // //     }
 
 // // //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 // // // }
+// //  using System.Collections.Generic;
+// //  using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 // // using System;
-// // using System.Collections.Generic;
 // // using Microsoft.EntityFrameworkCore;
-// // using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
-// // namespace TodoApi;
-
-// // public partial class ToDoDbContext : DbContext
+// // namespace TodoApi
 // // {
-// //     // DbSet של משתמשים
-// //     public virtual DbSet<User> Users { get; set; }
-
-// //     public virtual DbSet<Item> Items { get; set; }
-
-// //     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-// //         : base(options)
+// //     public partial class ToDoDbContext : DbContext
 // //     {
+// //         public virtual DbSet<User> Users { get; set; }
+// //         public virtual DbSet<Item> Items { get; set; }
+
+// //         public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+// //             : base(options)
+// //         {
+// //         }
+
+// //         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// //         {
+// //             Console.WriteLine("OnConfiguring is being called");
+
+// //             try
+// //             {
+// //                 var connectionString = "Server=bd2ivxjpmx5a7gpoxc5j-mysql.services.clever-cloud.com;User=uyb9xnwwuyygrbpc;Password=crvceLArTRBtAmA8rhKG;Database=bd2ivxjpmx5a7gpoxc5j;";
+// //                 optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+// //                 Console.WriteLine("Database connection success");
+// //             }
+// //             catch (Exception ex)
+// //             {
+// //                 Console.WriteLine($"Database connection failed: {ex.Message}");
+// //                 throw; // Optionally rethrow for further handling
+// //             }
+// //         }
+
+// //         protected override void OnModelCreating(ModelBuilder modelBuilder)
+// //         {
+// //             modelBuilder
+// //                 .UseCollation("utf8mb4_0900_ai_ci")
+// //                 .HasCharSet("utf8mb4");
+
+// //             modelBuilder.Entity<Item>(entity =>
+// //             {
+// //                 entity.HasKey(e => e.Id).HasName("PRIMARY");
+// //                 entity.ToTable("items");
+// //                 entity.Property(e => e.Name).HasMaxLength(100);
+// //             });
+
+// //             modelBuilder.Entity<User>(entity =>
+// //             {
+// //                 entity.HasKey(e => e.Id).HasName("PRIMARY");
+// //                 entity.ToTable("users");
+// //                 entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
+// //                 entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
+// //             });
+
+// //             OnModelCreatingPartial(modelBuilder);
+// //         }
+
+// //         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// //     }
 // //     }
 
-// //     // הגדרת חיבור למסד הנתונים
-// //     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// // {
-// //     Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
-
-
-// //     try
-// // {
-// //     var connectionString = "Server=bd2ivxjpmx5a7gpoxc5j-mysql.services.clever-cloud.com;User=uyb9xnwwuyygrbpc;Password=crvceLArTRBtAmA8rhKG;Database=bd2ivxjpmx5a7gpoxc5j;";
-// //     //Configuration.GetConnectionString("ToDoDB");
-// //     optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-// //     Console.WriteLine("success");
-// // }
-// // catch (Exception ex)
-// // {
-// //     Console.WriteLine($"Database connection failed: {ex.Message}");
-// //     Console.WriteLine("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-// // }
-
-
-// // }
-
-// //     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-// //     //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-
-// //     // אפיון טבלאות ותכונות
-// //     protected override void OnModelCreating(ModelBuilder modelBuilder)
-// //     {
-// //         modelBuilder
-// //             .UseCollation("utf8mb4_0900_ai_ci")
-// //             .HasCharSet("utf8mb4");
-
-// //         // הגדרת טבלת Items
-// //         modelBuilder.Entity<Item>(entity =>
-// //         {
-// //             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-// //             entity.ToTable("items");
-
-// //             entity.Property(e => e.Name).HasMaxLength(100);
-// //         });
-
-// //         // הגדרת טבלת Users (למשתמשים)
-// //         modelBuilder.Entity<User>(entity =>
-// //         {
-// //             entity.HasKey(e => e.Id).HasName("PRIMARY");
-
-// //             entity.ToTable("users");
-
-// //             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
-// //             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
-// //         });
-
-// //         OnModelCreatingPartial(modelBuilder);
-// //     }
-
-// //     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-// // }
-//  using System.Collections.Generic;
-//  using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
-// using System;
+//     using System;
+// using System.Collections.Generic;
 // using Microsoft.EntityFrameworkCore;
+// using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
-// namespace TodoApi
+// namespace TodoApi;
+
+// public partial class ToDoDbContext : DbContext
 // {
-//     public partial class ToDoDbContext : DbContext
+//     // DbSet של משתמשים
+//     public virtual DbSet<User> Users { get; set; }
+
+//     public virtual DbSet<Item> Items { get; set; }
+
+//     public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
+//         : base(options)
 //     {
-//         public virtual DbSet<User> Users { get; set; }
-//         public virtual DbSet<Item> Items { get; set; }
-
-//         public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-//             : base(options)
-//         {
-//         }
-
-//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//         {
-//             Console.WriteLine("OnConfiguring is being called");
-
-//             try
-//             {
-//                 var connectionString = "Server=bd2ivxjpmx5a7gpoxc5j-mysql.services.clever-cloud.com;User=uyb9xnwwuyygrbpc;Password=crvceLArTRBtAmA8rhKG;Database=bd2ivxjpmx5a7gpoxc5j;";
-//                 optionsBuilder.UseMySql(connectionString, Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-//                 Console.WriteLine("Database connection success");
-//             }
-//             catch (Exception ex)
-//             {
-//                 Console.WriteLine($"Database connection failed: {ex.Message}");
-//                 throw; // Optionally rethrow for further handling
-//             }
-//         }
-
-//         protected override void OnModelCreating(ModelBuilder modelBuilder)
-//         {
-//             modelBuilder
-//                 .UseCollation("utf8mb4_0900_ai_ci")
-//                 .HasCharSet("utf8mb4");
-
-//             modelBuilder.Entity<Item>(entity =>
-//             {
-//                 entity.HasKey(e => e.Id).HasName("PRIMARY");
-//                 entity.ToTable("items");
-//                 entity.Property(e => e.Name).HasMaxLength(100);
-//             });
-
-//             modelBuilder.Entity<User>(entity =>
-//             {
-//                 entity.HasKey(e => e.Id).HasName("PRIMARY");
-//                 entity.ToTable("users");
-//                 entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
-//                 entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
-//             });
-
-//             OnModelCreatingPartial(modelBuilder);
-//         }
-
-//         partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
-//     }
 //     }
 
-    using System;
+//     // הגדרת חיבור למסד הנתונים
+//     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+// {
+//     Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
+
+
+//     try
+// {
+//       optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+//               Console.WriteLine("success"); // לוג פשוט
+
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Database connection failed: {ex.Message}");
+// }
+
+
+// }
+
+//     // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//     //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+
+//     // אפיון טבלאות ותכונות
+//     protected override void OnModelCreating(ModelBuilder modelBuilder)
+//     {
+//         modelBuilder
+//             .UseCollation("utf8mb4_0900_ai_ci")
+//             .HasCharSet("utf8mb4");
+
+//         // הגדרת טבלת Items
+//         modelBuilder.Entity<Item>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("items");
+
+//             entity.Property(e => e.Name).HasMaxLength(100);
+//         });
+
+//         // הגדרת טבלת Users (למשתמשים)
+//         modelBuilder.Entity<User>(entity =>
+//         {
+//             entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+//             entity.ToTable("users");
+
+//             entity.Property(e => e.Username).HasMaxLength(50).IsRequired();
+//             entity.Property(e => e.Password).HasMaxLength(255).IsRequired();
+//         });
+
+//         OnModelCreatingPartial(modelBuilder);
+//     }
+
+//     partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+// }
+
+using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
@@ -201,26 +277,7 @@ public partial class ToDoDbContext : DbContext
 
     // הגדרת חיבור למסד הנתונים
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-{
-    Console.WriteLine("OnConfiguring is being called"); // לוג פשוט
-
-
-    try
-{
-      optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
-              Console.WriteLine("success"); // לוג פשוט
-
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Database connection failed: {ex.Message}");
-}
-
-
-}
-
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    //     => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
+        => optionsBuilder.UseMySql("name=ToDodb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.40-mysql"));
 
     // אפיון טבלאות ותכונות
     protected override void OnModelCreating(ModelBuilder modelBuilder)
